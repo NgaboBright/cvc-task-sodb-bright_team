c77f2cfe43c29cb8a4e5c06e7de3a17d7aa4d566
NgaboBright <brightngabo68@gmail.com>
2025-03-13 18:36:27 +0200

    all

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* script.js */
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById("theme-toggle");
+    
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+        themeToggle.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+    });
+});
